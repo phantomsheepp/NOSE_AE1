c7a4393da87f4f00b2b4632449f9930d2ec6cfb3
elliottatkinson <149013000+elliottatkinson@users.noreply.github.com>
2023-10-26 11:33:31 +0100

    Comments/reports added to server.py
    I've put in very basic comments that are outlined by the specification above what I've deem the 'relevant' parts. Not sure what more you think might be necessary but I think anymore would be overkill. 
    
    I've also added reports (a bunch of print statements) after each request as outlined in the specification (so it includes hostname, port, success or failure, the request and if it failed, the reason for doing so. Will need more error checking though.

--- server/server.py
+++ server/server.py
@@ -6,16 +6,22 @@ sys.path.append('..')
 from shared_process import send_file, recv_file
 sys.path.append('server')
  
+# Creating TCP server socket
+srv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 
-srv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
 try:
-    hostname = "0.0.0.0" #IP Address we were told to use in specification
+    hostname = "0.0.0.0" #ip Address we were told to use in specification
     port = int(sys.argv[1])
-    print(f"Server up and running on {hostname}:{port}")
+ 
+    # Binding server to user defined port number
     srv_sock.bind((hostname, port))
 
+    # Reporting success
+    print(f"Server up and running on {hostname}:{port}")
+
+    # Waiting for client connections
     srv_sock.listen(5) #the number is how many clients can interact with server (starting
     #with 0) not sure how many the specification actually wants, couldn't see anything
 
@@ -27,39 +33,55 @@ except Exception as e:
 
 while True:
     try:
+
+        # Connecting client's socket and address to server
         cli_sock, cli_addr = srv_sock.accept()
         print(f"Client {cli_addr} connected.")
 
         data = cli_sock.recv(4096)
 
+        # Parsing request
         choice_list = data.decode().split(" ")
         choice = str(choice_list[0])
 
+        # Serving requests "list", "get", or "put" as given by client argument
+
+        # "list" argument requests a list of first level directory contents
         if choice == "list":
             # Retrieve directory contents and convert into correct format
             dir_contents_list = os.listdir()
             dir_contents_str = "\n".join(dir_contents_list)
             cli_sock.sendall(str.encode(dir_contents_str))
+            print(f"First level directory contents from the server {hostname}:{port} successfully returned to client.")
 
+        # "get" argument downloads file
         elif choice == "get":
             filename = str(choice_list[1])
-            if filename not in os.listdir():  #Check if file exists
-                raise Exception("File not found")
+
+            # Check if file exists
+            if filename not in os.listdir():  
+                raise Exception(f"File {filename} failed to download from the server {hostname}:{port} as it was not found.")
             else:
                 send_file(cli_sock, filename)
-            
+                print(f"File {filename} successfully downloaded from the server {hostname}:{port}.")
+
+        # "put" argument uploads file
         elif choice == "put":
             filename = str(choice_list[1])
 
-            if filename in os.listdir(): # Check if file exists
-                raise Exception("File already exists")
+            # Check if file exists
+            if filename in os.listdir(): 
+                raise Exception(f"File {filename} failed to upload to the server {hostname}:{port} as that file already exists.")
             else:
                 recv_file(cli_sock, filename)
+                print(f"File {filename} successfully uploaded to the server {hostname}:{port}.")
 
 
+    # Catchall exception in case of larger error
     except Exception as e:
         print(e)  
 
+    # Close client connection
     finally:
         cli_sock.close()
 
