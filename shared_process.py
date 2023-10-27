21183da5b719eb98d9b7e9bd9c2e2e5174124fda
phantomsheepp <147084355+phantomsheepp@users.noreply.github.com>
2023-10-27 07:17:28 +0100

    Update shared_process.py

--- shared_process.py
+++ shared_process.py
@@ -1,79 +1,118 @@
 import os 
 
+# Delimiter for the end of file and directory transfers
+DELIMITER = b"#END#"
+
 def valid_filename(name): 
+    """Ensures a valid file name has been inputted"""
+
     if "." not in name:
-        print("No file extension found.")
-        return False
-    elif len(name) > 50:
-        print("File name too long. Are you sure this is right?")
-        return False
+        return (False,"No file extension found.")
     
-    return True
+    return (True,"")
 
+def status_message(status, addr, type, filename="", error=""): 
+    """Generates standardly formatted status messages to describe the result of a request"""
 
-# Read file with given filename and send data over network via provided socket
+    if type == "list":
+        if error == "":
+            return f"{status} for '{type}' request -> Connected to: {addr}"
+        else:
+            return f"{status} for '{type}' request -> Connected to: {addr} -> Error: {error}"
+        
+    else:
+        if error == "":
+            return f"{status} for '{type}' request -> Connected to: {addr} -> File name: '{filename}'"
+        else:
+            return f"{status} for '{type}' request -> Connected to {addr} -> File name: '{filename}' -> Error: {error}"
+       
+
 def send_file(socket, filename, addr):
+    """Read file with given filename and send data over network via provided socket"""
+
     try:
+        # Read entire file in binary mode
         with open(filename,"rb") as file:
             data = file.read()
+
+            # Sends data with delimiter on the end
+            data += DELIMITER
             socket.sendall(data)
 
-        print(f"File {filename} successfully sent to {addr}.")
+        return (True,"")
 
-    except BrokenPipeError:
-        print(f"Connection broken to {addr} so send failed.")
+    except ConnectionResetError:
+        return (False, "Connection broken so send failed.")
 
     except Exception as e:
-        print(e)
-        print(f"File {filename} failed to send to {addr}.")
+        return (False, e)
 
 
-# Create file with given filename and write to it data received via provided socket
 def recv_file(socket, filename, addr):
+    """Create file with given filename and write to it data received via provided socket"""
+
     try:
         file_contents = bytearray()
-        data = socket.recv(4096)
-        file_contents += data
 
-        while len(data) > 0:
-            data = socket.recv(4096)
-            file_contents += data
+        # Receives file contents
+        while True:
+            data = socket.recv(1024)
+            if data:
+                file_contents += data
+            if not data or len(data) < 1024:
+                break
+        
+        # Checks that delimiter is on transfer
+        if file_contents[-5:] != DELIMITER:
+            raise Exception("File transfer incomplete.")
 
+        # Writes file data in exlusive creation, binary mode
         with open(filename,"xb") as file:
-            file.write(file_contents)
+            file.write(file_contents[:-5])
 
-        print(f"File {filename} successfully downloaded from {addr}.")
+        return (True,"")
 
-    except BrokenPipeError:
-        print(f"Connection broken to {addr} so download failed.")
+    except ConnectionResetError:
+        return (False,"Connection broken so download failed.")
 
     except Exception as e:
-        print(e)
-        print(f"File {filename} failed to download from {addr}. Make sure the file exists.")
+        return (False, e)
+    
 
-
-# Generate and send directory listing from server to client via provided socket
 def send_listing(socket, addr):
+    """Generate and send directory listing from server to client via provided socket"""
+
     try:
+        # Puts directory contents into a string
         dir_contents_list = os.listdir()
         dir_contents_str = "\n".join(dir_contents_list)
-        print(dir_contents_str)
-        socket.sendall(str.encode(dir_contents_str))
-        print(f"First level directory contents from the server successfully returned to client {addr}.")
 
+        # Sends with delimeter on the end
+        socket.sendall(str.encode(dir_contents_str) + DELIMITER)
+        
+        return (True,"")
+    
     except Exception as e:
-        print(e)
-        print(f"First level directory contents from the server failed to send to client {addr}.")    
+        return (False,e)
+    
 
-# Recieve listing from server via provided socket and print it
 def recv_listing(socket, addr):
+    """Receive listing from server via provided socket and print it"""
+
     try:
         data = socket.recv(4096)
-        directory_list = data.decode()
+
+        # Checks that delimiter is on the transfer
+        if data[-5:] != DELIMITER:
+            raise Exception("Directory transfer incomplete.")
+
+        # Prints directory contents
+        directory_list = data[:-5].decode()
         print("\nContents of directory: ")
         print(directory_list)
-        print(f"\nFirst level directory contents from the server {addr} successfully returned to client.")
+        print()
+        
+        return (True, "")
 
     except Exception as e:
-        print(e)
-        print(f"First level directory contents from the server failed to sent to client {addr}.")    
+        return (False,e)
